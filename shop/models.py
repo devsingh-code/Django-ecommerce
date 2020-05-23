01c79dfb52e945314ef31ddb8317122fe8f3637c
dev <devsingh6n@gmail.com>
2020-05-23 23:35:22 +0530

    Completed Bootstraping Shopping site app

--- shop/models.py
+++ shop/models.py
@@ -37,5 +37,8 @@ class Product(models.Model):
         verbose_name='product'
         verbose_name_plural = 'products'
 
+    def get_url(self):
+        return reverse('shop:prodCatDetails',args=[self.category.slug,self.slug])
+
     def __str__(self):
         return '{}'.format(self.name)
--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
 
 def allProdCat(request,c_slug=None):
     c_page= None
-    products=None
+    products_list=None
     if c_slug!=None:
         c_page=get_object_or_404(Category,slug=c_slug)
-        products = Product.objects.filter(category=c_page,available=True)
+        products_list = Product.objects.filter(category=c_page,available=True)
     else:
-        products =Product.objects.all().filter(available=True)
+        products_list =Product.objects.all().filter(available=True)
+        '''Paginator code'''
+        paginator= Paginator(products_list,6)
+        try:
+            page= int(request.GET.get('page','1'))
+        except:
+            page=1
+        try:
+            products=paginator.page(page)
+        except(InvalidPage,EmptyPage):
+            products=paginator.page(paginator.num_pages)
+
     return render(request,'shop/category.html',{'category':c_page,'products':products})
 
 def prodCatDetails(request,c_slug,product_slug):
     try:
-        product=Product.objects.get(categr_slug=c_slug,slug=product_slug)
+        product=Product.objects.get(category__slug=c_slug,slug=product_slug)
     except Exception as e:
         raise e
 
